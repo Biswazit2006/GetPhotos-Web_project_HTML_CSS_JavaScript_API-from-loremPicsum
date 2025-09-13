5dfec423363188d2e01c1b6e700f60607d66dea4
Biswazit <biswasbiswazit@gmail.com>
2025-09-13 17:42:44 +0600

    ghfgh

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
-const urlApi = 'https://picsum.photos/200/300'
+const photoContainer = document.querySelector('.photo_container')
+const urlApi = 'https://picsum.photos/v2/list'
 const Picture = ()=>{
-    fatch(urlApi).then(data=>data.json()).then((iteam=>{
-        console.log(iteam)
-    }))
+    fetch(urlApi).then(data=>data.json()).then((iteam=>{
+        console.log(iteam);
+
+        iteam.forEach(element => {
+            
+        const newImg = document.createElement('img')
+        newImg.src = element.download_url;
+        console.log(newImg)
+
+        const newAuth = document.createElement('p')
+        const Auth = newAuth.innerHTML=element.author;
+        // console.log(Auth)
+
+        const imgCard = document.createElement('div');
+        imgCard.classList.add('img_card');
+        imgCard.appendChild(newImg)
+        imgCard.appendChild(newAuth)
+
+
+        photoContainer.appendChild(imgCard);
+            
+        });
+
+    }));
 }
+
+Picture();
